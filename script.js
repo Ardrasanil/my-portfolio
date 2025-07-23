e612e77c3082e3b8576a4337b2e0442f86804617
Ardrasanil <145931496+Ardrasanil@users.noreply.github.com>
2025-07-23 23:55:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const typewriterTexts = [
+  "Creative Thinker ",
+  "Problem Solver ",
+  "Figma Expert",
+  "Visual Storyteller",
+  "Design to Reality "
+];
+
+let currentText = "";
+let textIndex = 0;
+let charIndex = 0;
+const speed = 100;
+const target = document.getElementById("typewriter-text");
+
+function type() {
+  if (charIndex < typewriterTexts[textIndex].length) {
+    currentText += typewriterTexts[textIndex].charAt(charIndex);
+    target.innerHTML = currentText;
+    charIndex++;
+    setTimeout(type, speed);
+  } else {
+    setTimeout(erase, 1500);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    currentText = currentText.slice(0, -1);
+    target.innerHTML = currentText;
+    charIndex--;
+    setTimeout(erase, speed / 2);
+  } else {
+    textIndex = (textIndex + 1) % typewriterTexts.length;
+    setTimeout(type, speed);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(type, 1000);
+});
+window.addEventListener("load", function() {
+  const loader = document.getElementById("loader");
+  loader.style.opacity = 0;
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 500);
+});
